d49a03ec4d557a4bf0a7b5098d2112fa745804fd
Stuart Izon <stuart.izon@gmail.com>
2020-04-28 01:33:18 +1000

    Fix an error in the route 53 record resource

--- main.tf
+++ main.tf
@@ -12,13 +12,13 @@ resource "aws_route53_record" "certificate_validation" {
   count           = length(var.subject_alternative_names) + 1
   name            = aws_acm_certificate.certificate.domain_validation_options[count.index].resource_record_name
   type            = aws_acm_certificate.certificate.domain_validation_options[count.index].resource_record_type
-  records         = aws_acm_certificate.certificate.domain_validation_options[count.index].resource_record_value
+  records         = [aws_acm_certificate.certificate.domain_validation_options[count.index].resource_record_value]
   zone_id         = var.zone_id
   ttl             = var.ttl
   allow_overwrite = true
 }
 
 resource "aws_acm_certificate_validation" "certificate" {
-  certificate_arn = aws_acm_certificate.certificate.arn
+  certificate_arn         = aws_acm_certificate.certificate.arn
   validation_record_fqdns = aws_route53_record.certificate_validation[*].fqdn
 }
